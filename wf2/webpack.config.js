d9e44c1a2563b717eac68d249009e88af8d60ab6
Apollo Tang <tang.apollo@gmail.com>
2016-03-26 13:44:02 -0400

    [madewithlove] use commonChunksPlugin

--- wf2/webpack.config.js
+++ wf2/webpack.config.js
@@ -1,3 +1,5 @@
+webpack = require('webpack');
+
 module.exports = {
     entry: './src',
     output: {
@@ -22,5 +24,21 @@ module.exports = {
                 loader: 'html'
             }
         ]
-    }
+    },
+    // Plugins differ from loaders in the sense that instead
+    // of only executing on a certain set of files, and being
+    // more of a “pipe”, they execute on all files and perform
+    // more advanced actions, that aren’t necessarily related
+    // to transformations.
+    plugins: [
+        // CommonChunksPlugin: it analyzes your chunks for recurring
+        // dependencies, and extracts them somewhere else. It
+        // can be a completely separate file (like vendor.js) or
+        // it can be your main file.
+        new webpack.optimize.CommonsChunkPlugin({
+            name:      'main', // Move dependencies to our main file
+            children:  true, // Look for common dependencies in all children,
+            minChunks: 2, // How many times a dependency must come up before being extracted
+        }),
+    ]
 };
