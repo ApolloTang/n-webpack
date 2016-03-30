6ee4af4cce7dc5fd0bf3b99ff48f4aa1f42722c5
Apollo Tang <tang.apollo@gmail.com>
2016-03-30 12:46:58 -0400

    [10] added style modules

---
+++ 10-pro-react/03-style-modules/webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    devtool: 'source-map',
+    devServer: {
+        contentBase: "./public",
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        port: 9999
+    },
+    entry:  __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                // To install: npm i json-loader -D
+                test: /\.json$/,
+                loader: "json"
+            },
+            {
+                // To install: npm i -D babel-core babel-loader babel-preset-es2015 babel-preset-react
+                test    : /\.jsx?$/,
+                exclude : /node_modules/,
+                loader  : 'babel',
+                // query   : { presets : ['es2015','react'] }  // required if not using .babalrc
+            },
+            {
+                // To install: npm i -D style-loader css-loader
+                test: /\.css$/,
+                loader: 'style!css?modules'
+                                    // ^-- load class names as unique id from CSS module into the consuming component,
+                                    //     locally scoped, and never have to worry about css class name conflict
+            }
+        ]
+    },
+}
